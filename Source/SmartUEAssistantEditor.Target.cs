576477d0b4008d84fbd3777d432667ac5667b4a6
everfid-ever <166227111+everfid-ever@users.noreply.github.com>
2026-03-18 00:58:59 +0800

    feat: 从 SmartUEAssistant 迁移的 C++ 工具

--- Source/SmartUEAssistantEditor.Target.cs
+++ Source/SmartUEAssistantEditor.Target.cs
@@ -10,6 +10,6 @@ public class SmartUEAssistantEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
-		ExtraModuleNames.Add("SmartUEAssistant");
+		
 	}
 }
